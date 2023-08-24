d0d2f2db7eccdf16e1f0363d4e266cd651fd574d
Tobias Winkel <winkeltobias@gmail.com>
2023-08-24 08:16:35 +0200

    2nd

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function AlbumList({
   albums,
@@ -8,6 +9,37 @@ function AlbumList({
   editingAlbumId,
   selectedAlbumId,
 }) {
+  const [customerName, setCustomerName] = useState('');
+  const [customerEmail, setCustomerEmail] = useState('');
+
+  const handleCustomerNameChange = (event) => {
+    setCustomerName(event.target.value);
+  };
+
+  const handleCustomerEmailChange = (event) => {
+    setCustomerEmail(event.target.value);
+  };
+
+  const handleInterestSubmission = async (albumId) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:8080/api/albums/${albumId}/register-interest`,
+        {
+          name: customerName,
+          email: customerEmail,
+        }
+      );
+
+      if (response.status === 200) {
+        console.log('Customer interest registered successfully');
+        setCustomerName('');
+        setCustomerEmail('');
+        // Optionally, you can update the albums list or interest count
+      }
+    } catch (error) {
+      console.error('Error registering customer interest:', error);
+    }
+  };
   return (
     <table>
       <thead>
@@ -36,6 +68,28 @@ function AlbumList({
                   <button onClick={() => onEditClick(album.id)}>Edit</button>
                   <button onClick={() => onDeleteClick(album.id)}>Delete</button> {/* Add delete button */}
                   <button onClick={() => onRegisterInterestClick(album.id)}>Register Interest</button> {/* Add register interest button */}
+                </div>
+              )}
+            </td>
+            <td>
+            {selectedAlbumId === album.id && (
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    value={customerName}
+                    onChange={handleCustomerNameChange}
+                  />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    value={customerEmail}
+                    onChange={handleCustomerEmailChange}
+                  />
+                  
+                  <button onClick={() => handleInterestSubmission(album.id)}>
+                    Submit Interest
+                  </button>
                 </div>
               )}
             </td>
